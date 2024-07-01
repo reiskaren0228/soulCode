15bf4129a087ac16635e5e2e3d79ad31dc406985
reiskaren <kreisdemenezes@gmail.com>
2024-06-30 23:54:05 -0300

    feat: adicinado a resposta da tarefa do dia 28 de junho

--- JS/aula_28_06/objetos.js
+++ JS/aula_28_06/objetos.js
@@ -26,3 +26,29 @@ let pessoa2 = {
   altura: 1.68,
 };
 
+// Acesso de propriedade
+// console.log(pessoa1.nome);
+// console.log(pessoa2.idade);
+// console.log(pessoa1["peso"]);
+// console.log(pessoa2["altura"]);
+// console.log(pessoa1.email);
+
+// Adicionando propriedades ao objeto
+pessoa1["email"] = "jose@email.com";
+//console.log(pessoa1);
+
+// Alterar valores nas propriedades
+pessoa1.idade = 36;
+pessoa2["idade"] += 5; // acrescenta +5 a idade atual
+
+//console.log(pessoa1.idade);
+//console.log(pessoa2.idade);
+//console.log("Pessoa 2: ", pessoa2);
+
+// Removendo propriedades;
+delete pessoa1["idade"]; // pessoa1.idade
+//console.log("Pessoa 1: ", pessoa1);
+
+// Conversão JSON
+const dado = JSON.stringify(pessoa2);
+console.log(JSON.parse(dado));
